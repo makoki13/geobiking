6aca41bf4cbed081a55d0a7f6fd1c3d1c02e16af
Makoki <elcorreodepabloprieto@gmail.com>
2022-12-26 14:24:43 +0100

    neteja

--- procesa_gpx.php
+++ procesa_gpx.php
@@ -1,58 +1,6 @@
 <?php
 
 function get_localidad($lat, $lon) {
-    //https://nominatim.openstreetmap.org/reverse?lat=38.91464854439443&lon=-0.26150098500160646&zoom=10&format=jsonv2
-    $url = 'https://nominatim.openstreetmap.org/reverse?lat=' . $lat . '&lon=' . $lon . '&zoom=10&format=jsonv2';
-
-    // Initialize a new cURL session
-    $ch = curl_init($url);
-    var_dump($ch);
-
-    // Set cURL options
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);  // Return the response as a string
-    curl_setopt($ch, CURLOPT_HEADER, 0);  // Do not include the HTTP header in the response
-
-    // Send the request and get the response
-    $response = curl_exec($ch);
-    var_dump($response);
-
-    // Close the cURL session
-    curl_close($ch);
-
-    // Parse the response string into a PHP object
-    $data = json_decode($response);
-
-    //var_dump($data);
-}
-
-function get_localidad_get($lat, $lon) {
-    //$url = 'https://nominatim.openstreetmap.org/reverse?lat=' . $lat . '&lon=' . $lon . '&zoom=10&format=jsonv2';
-    $url = 'http://nominatim.openstreetmap.org/reverse?lat=38.91464854439443&lon=-0.26150098500160646&zoom=10&format=jsonv2';
-
-    // Send the HTTP request and get the response as a string
-    $response = file_get_contents($url);
-
-    // Parse the response string into a PHP object
-    $data = json_decode($response);
-
-    var_dump($data);
-}
-
-function get_localidad_json($lat, $lon) {
-    $json = "https://nominatim.openstreetmap.org/reverse?format=json&lat=".$lat."&lon=".$lon."&zoom=27&addressdetails=1";
-
-    $ch = curl_init($json);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:59.0) Gecko/20100101 Firefox/59.0");
-    $jsonfile = curl_exec($ch);
-    var_dump($jsonfile);
-    curl_close($ch);
-
-    $RG_array = json_decode($jsonfile,true);
-    var_dump($RG_array);
-}
-
-function get_localidad_v4($lat, $lon) {
     $json = "https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=".$lat."&lon=".$lon."&zoom=10";
     $opts = [
         'http' => [
@@ -100,20 +48,13 @@ foreach ($gpx->trk as $trk) {
             $i++;
             $lat = (float) $trkpt['lat'];
             $lon = (float) $trkpt['lon'];
-            // Process the track point      
-            //get_localidad($lat, $lon);
-            //get_localidad_get($lat, $lon);
-            //get_localidad_json($lat, $lon);
-      
-            $poblacion = get_localidad_v4($lat, $lon);
+                  
+            $poblacion = get_localidad($lat, $lon);
             echo "lat: $lat . long: $lon -> poblacion $poblacion\n";
 
             $lista_poblaciones[$poblacion] = true;
 
-            //$poblacion = get_localidad_v4(38.91464854439443,-0.26150098500160646);
-            //echo "lat: $lat . long: $lon -> poblacion $poblacion\n";
-
-            if ($i>=100) {
+            if ($i>=10) {
                 exit();
             }
         }
