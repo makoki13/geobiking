e6728e007fb0feed18563b3c2c0d8288264573b6
makoki13 <pablo.makoki@gmail.com>
2023-01-23 16:54:18 +0100

    paths imagen correo

--- kks/kk_correo.php
+++ kks/kk_correo.php
@@ -8,7 +8,7 @@ $cuerpo = '<html><body><h1>Funciona!</h1></body></html>';
 mail($destinatario,$titulo,$cuerpo); */
 
 
-require_once "lib/TurboApiClient.php";
+require_once "./app/backend/api/lib/TurboApiClient.php";
 
 $titulo = 'test correo';
 $cuerpo = '<html><body><h1>Funciona!</h1></body></html>';
@@ -20,13 +20,13 @@ $email->setToList("elcorreodepabloprieto@gmail.com");
 //$email->setCcList("dd@domain.com,ee@domain.com");
 //$email->setBccList("ffi@domain.com,rr@domain.com");	
 $email->setSubject($titulo);
-$email->setContent($cuerpo);
+//$email->setContent($cuerpo);
 $email->setHtmlContent("html content");
-$email->addCustomHeader('X-FirstHeader', "value");
-$email->addCustomHeader('X-SecondHeader', "value");
-$email->addCustomHeader('X-Header-da-rimuovere', 'value');
-$email->removeCustomHeader('X-Header-da-rimuovere');
-$email->addAttachment('./files/turbosmtp.png');
+//$email->addCustomHeader('X-FirstHeader', "value");
+//$email->addCustomHeader('X-SecondHeader', "value");
+//$email->addCustomHeader('X-Header-da-rimuovere', 'value');
+//$email->removeCustomHeader('X-Header-da-rimuovere');
+$email->addAttachment('./app/backend/api/files/turbosmtp.png');
 
 
 $turboApiClient = new TurboApiClient("pablo.makoki@gmail.com", "_password");
